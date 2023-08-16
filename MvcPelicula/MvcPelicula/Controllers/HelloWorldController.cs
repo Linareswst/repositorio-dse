358b2e383da3cde8a738c5c9a9eb022f9af3bccd
linareswst <oscar.alejandro.linares@gmail.com>
2023-08-15 20:40:26 -0600

    Subiendo archivos de primera práctica

---
+++ MvcPelicula/MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            ViewBag.Mensaje = "Hola " + nombre+" "+apellido+ ", veces mostrado=";
+            ViewBag.numVeces = numVeces;
+
+            return View();
+        }
+    }
+}
